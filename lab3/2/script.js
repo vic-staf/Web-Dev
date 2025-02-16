cef00de0f83188349e470321fd2727293dddd8c0
vic-staf <156738807+vic-staf@users.noreply.github.com>
2025-02-16 12:00:00 +0500

    lab4

--- lab3/2/script.js
+++ lab3/2/script.js
@@ -1,45 +1,54 @@
-const todoInput = document.getElementById('input-text');
-const addButton = document.getElementById('add-button');
-const todoList = document.getElementById('list');
+const input = document.getElementById('input-text');
+const add = document.getElementById('add-button');
+const list = document.getElementById('list');
 
 
-addButton.addEventListener('click', () => {
-    const taskText = todoInput.value.trim();
-    if (taskText === '') {
+add.addEventListener('click', () => {
+    const text = input.value.trim();
+    if (text === '') {
         return;
     }
 
-    const newTask = document.createElement('li');
+    const task = document.createElement('li');
 
-    newTask.innerHTML = `
+
+    task.innerHTML = `
     <div class="item">
       <input type="checkbox" class="done">
-    <span>${taskText}</span>
+      <span>${text}</span>
       <button class="delete-button"><img src="bin.png"></button>
     </div>
   `;
 
-    todoList.appendChild(newTask);
-    todoInput.value = '';
+    list.appendChild(task);
+    input.value = '';
 
-    updateTaskActions();
+
+
+    updateButtons();
 });
 
-function updateTaskActions() {
-    const checkboxes = todoList.querySelectorAll('.done');
+
+
+function updateButtons() {
+    const checkboxes = list.querySelectorAll('.done');
     checkboxes.forEach((checkbox) => {
-        checkbox.addEventListener('change', (e) => {
-            const task = e.target.closest('li');
-            task.classList.toggle('done', e.target.checked);
+        checkbox.addEventListener('change', (element) => {
+            const li = element.target.closest('li');
+            li.classList.toggle('done');
         });
     });
 
-    const deleteButtons = todoList.querySelectorAll('.delete-button');
+    const deleteButtons = list.querySelectorAll('.delete-button');
     deleteButtons.forEach((button) => {
-        button.addEventListener('click', (e) => {
-            const task = e.target.closest('li');
-            task.remove();
+        button.addEventListener('click', (element) => {
+            const li = element.target.closest('li');
+            document.getElementById('deleted').appendChild(li);
+            // li.remove();
+
+
         });
+
     });
 }
-updateTaskActions();
+updateButtons();
---
+++ lab4/part1/first-app_01-hello-world/src/app/home/home.component.ts
@@ -0,0 +1,47 @@
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingocationComponent} from '../housingocation/housingocation.component';
+import {HousingLocation} from '../housinglocation';
+import {HousingService} from '../housing.service';
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, HousingocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housingocation
+          *ngFor="let housingLocation of filteredLocationList"
+          [housingLocation]="housingLocation"
+      ></app-housingocation>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
+  }
+}
+
+
+
